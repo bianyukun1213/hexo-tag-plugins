d3c023a57fbf9010f56ab6ff010f679b63cebbe6
边宇琨 <bianyukun1213@outlook.com>
2024-05-04 22:22:59 +0800

    Update tag-image-masonry.js

--- tag-image-masonry.js
+++ tag-image-masonry.js
@@ -1,3 +1,16 @@
+// https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+String.prototype.hashCode = function () {
+  let hash = 0,
+    i, chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr = this.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
 /*
     Image Masonry Tag: https://github.com/bigbite/macy.js
 
@@ -6,7 +19,9 @@
     
 */
 
-hexo.extend.tag.register('image_masonry', (args) => {
+let masonryCount = {};
+
+hexo.extend.tag.register('image_masonry', function (args) {
   let list = '';
   let imgCount = 0;
   const argsLength = args.length;
@@ -23,16 +38,23 @@ hexo.extend.tag.register('image_masonry', (args) => {
     imgCount++;
   }
   // 修改 id，并且使内联 javascript、macy 实例也附带 id，避免一个页面中放多个瀑布流报变量二次声明的错误。
-  const id = Math.random().toString(36).substring(2, 8);
+  const { path } = this;
+  const prefix = path.hashCode();
   // 默认 3 列，如果 list 里图片少于 3 个，以 list 里图片个数为准。
   // 如果不这样写，假如 list 里只有两个图片，则这两个图片会即在左 2/3 的位置，右 1/3 是空白，不好看。
-  // 需要注意的是，要在 site-mod.js 里重新计算 masonry，需要用 var 而不是 let 声明 macyAtxxx。
+  // 需要注意的是，要在 site-mod.js 里重新计算 masonry，需要用 var 而不是 let 声明 macyxxx。
+  let currentCount;
+  if (typeof masonryCount[prefix] === 'undefined')
+    currentCount = masonryCount[prefix] = 0;
+  else
+    currentCount = masonryCount[prefix] = masonryCount[prefix] + 1;
+  const id = `${(prefix + '').replace('-', 'n')}m${currentCount}`;
   const elements = `
     <div class="image-masonry" id="image-masonry-${id}">
       ${list}
     </div>
     <script class="image-masonry-script" id="image-masonry-script-${id}">
-      var macyAt${id} = new Macy({
+      var macy${id} = new Macy({
         container: '#image-masonry-${id}',
         trueOrder: false,
         waitForImages: false,
@@ -49,8 +71,8 @@ hexo.extend.tag.register('image_masonry', (args) => {
         }
       });
       // 未加载完成时，让所有图片不要叠在一行。
-      macyAt${id}.runOnImageLoad(() => {
-        macyAt${id}.recalculate(true);
+      macy${id}.runOnImageLoad(() => {
+        macy${id}.recalculate(true);
       }, true);
     </script>
   `;
